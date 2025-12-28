cef11bb2c56178c9c73f11f6d17f3cbb21f368aa
Marko Linke <marko.linke@gmail.com>
2025-12-28 14:52:22 +0100

    Fixed build and config issues

--- applications/frontend-app/src/app/App.vue.js
+++
@@ -1,9 +0,0 @@
-const __VLS_ctx = {};
-let __VLS_components;
-let __VLS_intrinsics;
-let __VLS_directives;
-/** @type {__VLS_StyleScopedClasses['logo']} */ ;
-/** @type {__VLS_StyleScopedClasses['logo']} */ ;
-__VLS_asFunctionalElement1(__VLS_intrinsics.div, __VLS_intrinsics.div)({});
-const __VLS_export = (await import('vue')).defineComponent({});
-export default {};
