3f3313f0369b81f2e88ab7a2458dbc06f4a958ac
Marko Linke <marko.linke@gmail.com>
2026-01-08 14:06:45 +0100

    FE | Created ButtonWithLoader common UI component

---
+++ applications/frontend-app/src/ui/base/ButtonWithLoader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { FwbButton, FwbSpinner } from 'flowbite-vue';
+
+    const props = defineProps<{
+        loading: boolean;
+    }>();
+</script>
+
+<template>
+  <fwb-button>
+    <div  class="flex items-center justify-between gap-2">
+        <slot></slot>
+        <fwb-spinner v-if="loading" class="ml-2" />
+    </div>
+  </fwb-button>
+</template>
